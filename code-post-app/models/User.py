c0eb8e2d68fa66a66109136c0cd558b057150833
zachary <zacharyberdell@gmail.com>
2022-03-22 15:56:12 -0600

    User model built.

--- code-post-app/models/User.py
+++ code-post-app/models/User.py
@@ -1,11 +1,26 @@
 from django.db import models
-import re
 import bcrypt
+from django.core.exceptions import ValidationError
+
+salt = bcrypt.gensalt()
+
+def validate_password(self, key, password):
+    if len(password) > 7:
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
+    else:
+        raise ValidationError('Your password must be at least 8 characters.')
+
+def verify_password(self, password):
+    return bcrypt.checkpw(
+        password.encode('utf-8'),
+        self.password.encode('utf-8')
+    )
 
 class User(models.Model):
     username = models.Charfield(max_length=48, unique=True)
-    email_address = models.CharField(max_length=320, unique=True)
-    password = models.CharField(max_length=64, min_length=8)
+    email_address = models.CharField(max_length=320, unique=True, validators=[validate_email])
+    isAdmin = models.BooleanField(default=False)
+    password = models.CharField(max_length=64, min_length=8, validators=[validate_password, verify_password])
 
     class Meta:
         app_label = 'code-post-app'
