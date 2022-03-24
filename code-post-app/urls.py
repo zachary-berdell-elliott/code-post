764b8d6ee91a9bca6f9c97d36769ce4a567db0e8
zachary <zacharyberdell@gmail.com>
2022-03-24 15:49:19 -0600

    Fixed the file structure

--- code-post-app/urls.py
+++ code-post-app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from rest_framework import routers
 
 urlpatterns = [
     path('profile', views.profile),
--- codepost/settings.py
+++ codepost/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,11 @@ WSGI_APPLICATION = 'codepost.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': getenv('DB_NAME'),
+        'USER': getenv('DB_USER'),
+        'PASSWORD': getenv('DB_PASSWORD'),
+        'HOST': getenv('DB_HOST')
     }
 }
 
