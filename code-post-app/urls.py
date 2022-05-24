a14a060c8d7ef6b7c1a7cc1b87677ad4f22486f9
zachary <zacharyberdell@gmail.com>
2022-05-24 12:44:37 -0600

    Added more to the urls.

--- code-post-app/urls.py
+++ code-post-app/urls.py
@@ -5,9 +5,11 @@ from rest_framework import routers
 app_name='code-post-app'
 
 urlpatterns = [
-    path('profile', views.profile, name='profile'),
-    path('', views.homepage, name='homepage'),
-    path('language/<str>', views.language, name='language'),
-    path('snippet/<int:pk>', views.snippet, name=snippet),
-    path('user/<int:pk>', views.user_profile, name='user-profile')
+    path('/profile', views.profile, name='profile'),
+    path('/', views.homepage, name='homepage'),
+    path('/language/<str>', views.language, name='language'),
+    path('/snippet/<int:pk>', views.snippet, name='snippet'),
+    path('/user/<int:pk>', views.user_profile, name='user-profile'),
+    path('/login', views.login, name='login'),
+    path('/signup', views.signup, name='signup')
 ]
